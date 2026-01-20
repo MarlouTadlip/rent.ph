aacbb794bcad50153886a91af6759adae9bad07a
Beirun <orillobernardjay@gmail.com>
2026-01-20 17:50:29 +0800

    Add dark mode switch component

---
+++ app/components/darkModeSwitch.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+
+const colorMode = useColorMode()
+
+const changeMode = () => {
+  colorMode.preference = colorMode.preference === 'light' ? 'dark' : 'light'
+}
+</script>
+
+<template>
+  <Button variant="outline" @click="changeMode">
+    <Icon
+      name="radix-icons:moon"
+      class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+    />
+    <Icon
+      name="radix-icons:sun"
+      class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+    />
+    <span class="sr-only">Toggle theme</span>
+  </Button>
+  <!-- <DropdownMenu>
+    <DropdownMenuTrigger as-child> </DropdownMenuTrigger>
+    <DropdownMenuContent align="end">
+      <DropdownMenuItem @click="colorMode.preference = 'light'"> Light </DropdownMenuItem>
+      <DropdownMenuItem @click="colorMode.preference = 'dark'"> Dark </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu> -->
+</template>
