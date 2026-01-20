84efff1040ab723121970a27249c778770e4b11c
Beirun <orillobernardjay@gmail.com>
2026-01-20 17:50:29 +0800

    Add hero component

---
+++ app/components/hero.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const colorMode = useColorMode()
+const heroBg = computed(() => {
+  if (colorMode.preference === 'light') {
+    return `bg-[url('/hero.jpg')]`
+  } else {
+    return `bg-[url('/hero-dark.jpg')]`
+  }
+})
+</script>
+<template>
+  <div :class="['h-[70vh] bg-center bg-cover w-screen', heroBg]"></div>
+</template>
