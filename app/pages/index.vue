3749a45c96838ed10ba001d777b03c9265bba780
Beirun <orillobernardjay@gmail.com>
2026-01-20 15:17:43 +0800

    Update homepage

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts"></script>
 <template>
-    <div>
-        Rent
-    </div>
+  <div class="w-screen min-h-screen">
+    <ClientOnly>
+      <Navbar></Navbar>
+    </ClientOnly>
+    <div class="h-screen w-screen"></div>
+    <div class="h-screen w-screen"></div>
+  </div>
 </template>
-
