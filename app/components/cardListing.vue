a7a4b101b67d6f5ede292d60b371565c232c6953
Beirun <orillobernardjay@gmail.com>
2026-01-21 13:49:38 +0800

    Add card listing component

---
+++ app/components/cardListing.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="rounded-lg w-85 h-110 flex flex-col shadow-lg">
+    <div
+      class="h-full w-full bg-green-100 rounded-t-lg bg-[url('https://picsum.photos/400/300')] bg-bottom"
+    ></div>
+    <div class="h-full w-full"></div>
+  </div>
+</template>
