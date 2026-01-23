8a0d749e332d01b5b2eee85f30af474ed835a9b5
DarelBG <brylgorgonio@gmail.com>
2026-01-23 08:56:46 +0800

    Ambot unsai naa nako ari, pero ako lng e commit

--- app/pages/property.vue
+++ app/pages/property.vue
@@ -4,20 +4,24 @@ import navbar from '~/components/navbar.vue'
 </script>
 
 <template>
-    <div class="h-[95vh] bg-top bg-cover w-full flex flex-col items-center gap-5">
+    <div class="h-[95vh] bg-top bg-cover w-full flex flex-col items-center gap-5 text-[#262626]">
         <div class="w-full">
             <ClientOnly>
                 <navbar />
             </ClientOnly>
         </div>
-        <div class="flex-grow flex items-center justify-center mx-100">
-            <div>
-                <h1>Property for Rent</h1>
+        <div class="flex-grow flex justify-center mx-100 flex-col">
+            <div class="py-10">
+                <h1 class="text-left text-4xl font-bold">Property for Rent</h1>
             </div>
             <ClientOnly>
                 <propertySearchBar />
             </ClientOnly>
         </div>
-        
+        <div class="min-h-screen w-screen p-8">
+            <div class="flex flex-wrap justify-evenly gap-y-12 gap-x-2">
+                <CardListing v-for="_ in new Array(8)"></CardListing>
+            </div>
+         </div>
     </div>
 </template>
