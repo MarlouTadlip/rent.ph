51f58f6b62294e0fd487aebd55514d6c401b10ab
Beirun <orillobernardjay@gmail.com>
2026-01-26 15:36:59 +0800

    Update agent search bar component

--- app/components/agentSearchBar.vue
+++ app/components/agentSearchBar.vue
@@ -1,58 +1,58 @@
 <script setup lang="ts"></script>
 <template>
   <div
-    class="flex bg-[#fafafa] dark:bg-[#212121] p-6 rounded-lg gap-4 border dark:border-gray-800 border-gray-200 drop-shadow-md w-full max-w-8xl mx-auto"
+    class="flex bg-white dark:bg-[#212121] p-6 rounded-lg gap-4 border dark:border-gray-800 border-gray-200 drop-shadow-md w-full max-w-8xl mx-auto"
   >
     <div class="flex flex-col gap-4 flex-1">
-        <Label for="location">Enter Name</Label>
-        <Input
-          name="keyword"
-          class="w-full py-2 pl-3 border border-dark-100 rounded-md text-sm h-10.5"
-          icon="i-lucide-search"
-          placeholder="Name"
-        />
-      </div>
-      <div class="flex flex-col gap-4 flex-1">
-        <Label for="location">Province</Label>
-        <Select id="location">
-          <SelectTrigger class="w-full py-5">
-            <SelectValue>
-              <Icon name="lucide:map-pin" />
-              <Label>Select/Enter a Province</Label>
-            </SelectValue>
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectItem value="1">Option 1</SelectItem>
-              <SelectItem value="2">Option 2</SelectItem>
-              <SelectItem value="3">Option 3</SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-      </div>
-      <div class="flex flex-col gap-4 flex-1">
-        <Label for="location">City</Label>
-        <Select id="location">
-          <SelectTrigger class="w-full py-5">
-            <SelectValue>
-              <Icon name="lucide:map-pin" />
-              <Label>Select/Enter a City</Label>
-            </SelectValue>
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectItem value="1">Option 1</SelectItem>
-              <SelectItem value="2">Option 2</SelectItem>
-              <SelectItem value="3">Option 3</SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-      </div>
-      <div class="flex items-end">
-        <Button class="gap-2 px-8 py-5.25 min-w-[120px]">
-          <Icon name="lucide:search" class="bg-[#fafafa] dark:bg-[#121212]" />
-          Search
-        </Button>
-      </div>
+      <Label for="location">Enter Name</Label>
+      <Input
+        name="keyword"
+        class="w-full py-2 pl-3 border border-dark-100 rounded-md text-sm h-10.5"
+        icon="i-lucide-search"
+        placeholder="Name"
+      />
+    </div>
+    <div class="flex flex-col gap-4 flex-1">
+      <Label for="location">Province</Label>
+      <Select id="location">
+        <SelectTrigger class="w-full py-5">
+          <SelectValue>
+            <Icon name="lucide:map-pin" />
+            <Label>Select/Enter a Province</Label>
+          </SelectValue>
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectItem value="1">Option 1</SelectItem>
+            <SelectItem value="2">Option 2</SelectItem>
+            <SelectItem value="3">Option 3</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
+    <div class="flex flex-col gap-4 flex-1">
+      <Label for="location">City</Label>
+      <Select id="location">
+        <SelectTrigger class="w-full py-5">
+          <SelectValue>
+            <Icon name="lucide:map-pin" />
+            <Label>Select/Enter a City</Label>
+          </SelectValue>
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectItem value="1">Option 1</SelectItem>
+            <SelectItem value="2">Option 2</SelectItem>
+            <SelectItem value="3">Option 3</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
+    <div class="flex items-end">
+      <Button class="gap-2 px-8 py-5.25 min-w-[120px]">
+        <Icon name="lucide:search" class="bg-white dark:bg-[#121212]" />
+        Search
+      </Button>
+    </div>
   </div>
 </template>
