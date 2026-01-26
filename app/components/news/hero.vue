de922955b46299c297502a2f75fa7c7805965852
Beirun <orillobernardjay@gmail.com>
2026-01-26 11:59:39 +0800

    Update news hero component

--- app/components/news/hero.vue
+++ app/components/news/hero.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts"></script>
 <template>
-  <div :class="['h-[30vh] pt-10 w-screen flex items-center justify-center']">
-    <div class="text-6xl font-extrabold text-[#262626] dark:text-white/80">
+  <div :class="['pt-30 pb-20 w-screen flex items-center justify-center']">
+    <div
+      class="text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-extrabold text-[#262626] dark:text-white/80 w-full text-center"
+    >
       We Share Our Knowledge
     </div>
   </div>
