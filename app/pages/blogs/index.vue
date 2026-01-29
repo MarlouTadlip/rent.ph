5743de5bdacbc7080944d6c7de99b1db96668520
DarelBG <brylgorgonio@gmail.com>
2026-01-29 15:34:20 +0800

    Updated blog index

--- app/pages/blogs/index.vue
+++ app/pages/blogs/index.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 </script>
 <template>
-  <div class="w-screen min-h-screen bg-[url(/blogIndexBG.png)] bg-no-repeat bg-contain">
+  <div class="w-screen min-h-screen flex flex-col overflow-hidden">
     <ClientOnly>
       <Navbar />
     </ClientOnly>
+    <div class="w-screen h-screen bg-[url(/blogIndexBG.png)] bg-no-repeat bg-cover fixed -z-10"></div>
     <div id="blogsContainer">
       <NewsHero />
       <NewsHeader />
