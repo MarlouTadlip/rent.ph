d052f5568c0c3c82cf37a8ba2b91d506cf89a666
DarelBG <brylgorgonio@gmail.com>
2026-02-03 16:42:55 +0800

    adjusted sidebar for agents page

--- app/components/agentSearchBar.vue
+++ app/components/agentSearchBar.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 <template>
   <div
-    class="bg-white dark:bg-[#212121] p-4 md:p-6 rounded-lg border dark:border-gray-800 border-gray-200 drop-shadow-md w-full max-w-8xl mx-auto"
+    class="bg-white dark:bg-[#212121] p-4 md:p-6 rounded-lg border border-gray-300 dark:border-gray-800 border-gray-200 bg-gray-50 w-full max-w-8xl mx-auto"
   >
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:flex lg:flex-row gap-4 items-end">
       
@@ -10,7 +10,7 @@
         <Input
           id="name"
           name="keyword"
-          class="w-full border-gray-300 dark:border-gray-700 rounded-md h-10 md:h-12 focus-visible:ring-[#fe8e0a]"
+          class="w-full dark:border-gray-700 rounded-md h-10 md:h-12 focus-visible:ring-[#fe8e0a]"
           placeholder="Agent or Manager Name"
         />
       </div>
@@ -18,7 +18,7 @@
       <div class="flex flex-col gap-2 flex-1 w-full">
         <Label for="province" class="text-sm md:text-base font-medium">Province</Label>
         <Select id="province">
-          <SelectTrigger class="w-full h-10 md:h-12 border-gray-300 dark:border-gray-700">
+          <SelectTrigger class="w-full h-10 md:h-12 dark:border-gray-700">
             <SelectValue placeholder="Select Province" />
           </SelectTrigger>
           <SelectContent>
@@ -34,7 +34,7 @@
       <div class="flex flex-col gap-2 flex-1 w-full">
         <Label for="city" class="text-sm md:text-base font-medium">City</Label>
         <Select id="city">
-          <SelectTrigger class="w-full h-10 md:h-12 border-gray-300 dark:border-gray-700">
+          <SelectTrigger class="w-full h-10 md:h-12 dark:border-gray-700">
             <SelectValue placeholder="Select City" />
           </SelectTrigger>
           <SelectContent>
