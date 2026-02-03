5cdd716eb0478350422bac4915afd346971bba29
Beirun <orillobernardjay@gmail.com>
2026-02-03 17:08:12 +0800

    Update manager store logic

--- app/stores/managerStore.ts
+++ app/stores/managerStore.ts
@@ -1,11 +1,13 @@
 import type { Agent } from '~/models/manager'
 import type { Pagination } from '~/models/pagination'
+import type { Review } from '~/models/review'
 
 const BASE_URL = 'https://rent.ph/api'
 
 export const useManagerStore = defineStore('manager', () => {
   const manager = ref<Agent>()
   const managers = ref<Agent[]>([])
+  const reviews = ref<Review[]>([])
   const pros = computed(() => managers.value.filter((m) => m.rmpro === 'on'))
   const pagination = ref<Pagination>()
   const loading = ref(false)
@@ -13,7 +15,7 @@ export const useManagerStore = defineStore('manager', () => {
   const getManager = async (id: string | number) => {
     loading.value = true
     try {
-      const res = await fetch(`${BASE_URL}/rent-managers/${id}`)
+      const res = await fetch(`${BASE_URL}/rent-manager-detail/${id}`)
       if (!res.ok) throw new Error(`Failed to fetch manager: ${res.statusText}`)
       const json = await res.json()
       manager.value = json.data
@@ -44,6 +46,20 @@ export const useManagerStore = defineStore('manager', () => {
     }
   }
 
+  const getReviews = async (id: string | number) => {
+    loading.value = true
+    try {
+      const res = await fetch(`${BASE_URL}/user/${id}/reviews`)
+      if (!res.ok) throw new Error(`Failed to fetch reviews: ${res.statusText}`)
+      const json = await res.json()
+      reviews.value = json.data
+    } catch (err) {
+      console.error(err)
+    } finally {
+      loading.value = false
+    }
+  }
+
   return {
     manager,
     managers,
@@ -51,6 +67,8 @@ export const useManagerStore = defineStore('manager', () => {
     pagination,
     getManager,
     getManagers,
+    reviews,
+    getReviews,
     loading,
   }
 })
